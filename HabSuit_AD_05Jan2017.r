c270e5ee09fe995dd10863c6c408a613e605d26e
adroghini <droghini@ualberta.ca>
2017-01-05 10:37:50 -0900

    Update HabSuit_AD_05Jan2017.r

--- HabSuit_AD_05Jan2017.r
+++ HabSuit_AD_05Jan2017.r
@@ -1,6 +1,16 @@
 ##Habitat suitability
-
 #create binary suitability map for each species based on temp/sal tolerance
+
+##start up requirements
+require(dplyr)
+require(lattice)
+##Lattice set up
+panel.mean <- function(x, y, ...) {
+	cat('Mean', fill =T)
+    tmp <- tapply(x, y, FUN = mean); print(tmp)
+    panel.points(x=tmp, y=seq_along(tmp), ...)
+}
+cexFactor = 1
 
 #read files
 SpToler<-read.csv(file.path(baseDir, 'taxaData/Species_Tolerances.csv'))
@@ -9,39 +19,46 @@ ff<-list.files(path = file.path(baseDir, 'rData'))
 
 
 ###still need to loop all of this...
-MAX<-F
-f<-ff[4] ##hindcast temp data only - but use tony's loop in final code
-load(file.path(baseDir, 'rData', f)) 
+MAX<-F #set to T if want max temp instead of mean...
 
-##from ExploreData script
-ddPrimeWeeks<-tbl_df(sgdf.Max@data[, substring(names(sgdf.Max@data), 6,7) %in% as.character(20:40)]) ##sets weeks of interest 20-40
-sgdf_PW<-sgdf.Max ##Recreate sgdf similar to sgdf.Max, but only considering (mean/max) across prime weeks
+for(i in 1:ff[c(3,4)]){ ##load only hindcast sal and temp  
+	f = ff[i] 
+	cat('=== Handling ===', f, fill =T) 
+	load(file.path(baseDir, 'rData', f))
+	ddPrimeWeeks<-tbl_df(sgdf.Max@data[, substring(names(sgdf.Max@data), 6,7) %in% as.character(20:40)]) ##sets weeks of interest 20-40
+	sgdf_PW<-sgdf.Max ##creating prime week subset
+	
 	if(MAX){
 		ddPW_max <- data.frame(meanPW = apply(ddPrimeWeeks, 1, max, na.rm=TRUE)) ## Take the max across the prime weeks.
-		sgdf_PW@data <- ddPW_max
-		plotName <- paste0('Maximum', unlist(strsplit(f, ".rData")),  '.png')
-		theTitle <- paste0('Maximum', unlist(strsplit(f, ".rData")),  ' during the prime season')
+		sgdf_PW@data <- ddPW_max	
 	}else{
 		ddPW_mean <- data.frame(meanPW = apply(ddPrimeWeeks, 1, mean, na.rm=TRUE)) ## Take the mean across the prime weeks.
 		sgdf_PW@data <- ddPW_mean
-		plotName <- paste0(unlist(strsplit(f, ".rData")),  '.png') #plot filename becomes filename.png
-		theTitle <- paste0(unlist(strsplit(f, ".rData")),  ' during the prime season') #plot title becomes filename + prime season
 	}
-	
-	PW.r<-raster(sgdf_PW)
+	PW.sal<-raster(sgdf_PW) ##how to have for loop name files differently...
+}
+plot(PW.temp)
+plot(PW.sal)
 
 
-##working on it - for 1 raster only
-
+##working on it...	
 for (i in 1:length(SpToler$Sci_Name)){ 
 	##define tolerances
-	low<-SpToler$Min_TempC[i]
-	high<-SpToler$Max_TempC[i]
-
-	##reclassify raster into binary
-	threshold <- c(-Inf, low, 0,  low, high, 1,  high, Inf, 0)
-	tmat <- matrix(threshold, ncol=3, byrow=TRUE)
-	Toler.rast <- reclassify(PW.r, tmat)
+	lowT<-SpToler$Min_TempC[i]
+	highT<-SpToler$Max_TempC[i]
+	lowS<-SpToler$Min_Salinity[i]
+	highS<-SpToler$Max_Salinity[i]
+	
+	##reclassify sal & temp rasters into binary
+	thresT <- c(-Inf, lowT, 0,  lowT, highT, 1,  highT, Inf, 0)
+	Tmat <- matrix(thresT, ncol=3, byrow=TRUE)
+	rastTemp <- reclassify(PW.temp, Tmat)
+	thresS <- c(-Inf, lowS, 0,  lowS, highS, 1,  highS, Inf, 0)
+	Smat <- matrix(thresS, ncol=3, byrow=TRUE)
+	rastSal <- reclassify(PW.sal, Smat)
+	
+	#combine
+	combRast<-rastTemp*rastSal
 
 	##plot results
 	plotName <- paste0(unlist(strsplit(as.character(SpToler$Sci_Name[i])," ")), collapse = "_",'.png')
