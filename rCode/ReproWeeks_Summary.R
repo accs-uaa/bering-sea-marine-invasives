b9bae06df27383f32665bc5e5dba7d9c480e1fc1
Amanda Droghini <adroghini@alaska.edu>
2019-10-25 12:17:15 -0800

    Update ReproWeeks_Summary.R

--- rCode/ReproWeeks_Summary.R
+++ rCode/ReproWeeks_Summary.R
@@ -4,66 +4,80 @@
 ##Intent: How many species can reproduce under a) current and b) future conditions? (Assume minimum 1 week required)
 ##What is the frequency distribution for the # of consecutive weeks under a) current and b) future conditions?
 ##Maximum/average/etc. # of consecutive weeks?
-require(ggplot2)
-require(plyr)
-setwd(file.path(baseDir))
+
+#### Start up requirements----
+
+## Source initial run script
+source('rCode/init.R')
+
+## Load results of reproductive habitat suitability
 ff <- list.files(file.path(baseDir, 'rOut', 'Reproduction'), pattern = 'rData')
 
-##Create dataframe into which to place results
+#### Summary results for each taxon----
+# For each model & study period combination
+# Identify how many consecutive weeks of reproduction are available for each taxon
+# Number of weeks chosen as maximum pixel value in our study area, for that year-model combination
+
+## Create dataframe in which to place results
 nn <- c('Taxa', 'Model', 'StudyPeriod', 'noRepro', eval(paste0('year', 1:10)))
-repro <- data.frame(matrix(NA, ncol = length(nn), nrow = length(ff)))
-names(repro) <- nn
+resultsByTaxon <- data.frame(matrix(NA, ncol = length(nn), nrow = length(ff)))
+names(resultsByTaxon) <- nn
 rm(nn)
 
-##Loop through each taxa-model-study period to determine maximum consecutive weeks of suitable habitat
-for(r in 1:nrow(repro)){
+## Iterate through each taxon-model-study period combination
+
+for(r in 1:nrow(resultsByTaxon)){
   f <- ff[r]
-  ## parse out the taxa, model and study period for each file
-  repro$Taxa[r] <- unlist(strsplit(f, '_'))[1]
-  repro$Model[r] <- substr(unlist(strsplit(f, '_'))[3], 1, 5)
-  repro$StudyPeriod[r] <- substr(unlist(strsplit(f, '_'))[3], 7, 15)
-  
-  ##load each taxa-model-year combination
-  ##each .Rdata file s_OK.Repro contains 1 raster layer for every year in the study period
-  ##values of each cell representive number of consecutive weeks of suitable reproductive habitat 
-  ##based on temperature & salinity tolerances
-  
-  load(file.path(baseDir, 'rOut', 'Reproduction', f)) ##load file s_OK.Repro
-  cat(r, '=== Handling ===', f, fill =T) 
-  
-  repro.clip<-s_OK.Repro*Bering_200m  ##clip to Bering Sea continental shelf (see: BeringSea_Raster.R code)
-  
-  cs <- cellStats(repro.clip,max) ##extract maximum number of suitable weeks
-  ## place cell stats into year columns 
-  repro[r, eval(paste0('year', 1:length(cs))) ] <- cs
-  repro[r, 'noRepro'] <- all(cs == 0) ##if 0 for all years, noYearRoundSurvival == TRUE
-  
-  #calculate summaries
-  repro$Average<-rowMeans(repro[,5:14])
-  repro$stdev[r]=sd(repro[r,5:14])
+
+  # Parse out the taxon, model, and study period for the file
+  resultsByTaxon$Taxa[r] <- unlist(strsplit(f, '_'))[1]
+  resultsByTaxon$Model[r] <- substr(unlist(strsplit(f, '_'))[3], 1, 5)
+  resultsByTaxon$StudyPeriod[r] <- substr(unlist(strsplit(f, '_'))[3], 7, 15)
+
+  # Load file
+  # Each .Rdata file contains a raster stack (s_OK.Repro) that has one layer for every year in the study period (10 years per period)
+  # Pixel values represent the number of consecutive weeks of suitable reproductive habitat based on species' reproductive temperature and salinity requirements
+  load(file.path(baseDir, 'rOut', 'Reproduction', f))
+  cat(r, '=== Handling ===', f, fill =T)
+
+  # Clip to study area
+  reproClip <- s_OK.Repro * beringShelf
+
+  # For each year in study period, extract pixel with the highest value
+  # This represents the maximum number of weeks with suitable reproductive conditions found in at least one pixel in the Bering Sea
+  # Place values into appropriate 'year' columns in resultsByTaxon dataframe
+  # If max value = 0 for all years, column noRepro == TRUE
+
+  cs <- cellStats(reproClip,max)
+  resultsByTaxon[r, eval(paste0('year', 1:length(cs))) ] <- cs
+  resultsByTaxon[r, 'noRepro'] <- all(cs == 0)
+
+  # Calculate summary stats across all years in the study period
+  resultsByTaxon$Average<-rowMeans(resultsByTaxon[,5:14])
+  resultsByTaxon$stdev[r]=sd(resultsByTaxon[r,5:14])
 }
 
 #Create taxa ID key for graphing purposes
-Key<-as.data.frame(unique(repro$Taxa))
+Key<-as.data.frame(unique(resultsByTaxon$Taxa))
 Key$UID<-as.numeric(row.names(Key))
 colnames(Key)[1]<-"Taxa"
-repro<-merge(repro, Key, by='Taxa')
+resultsByTaxon<-merge(resultsByTaxon, Key, by='Taxa')
 rm(Key)
 
 ## save output
-fileName<-file.path(file.path(baseDir, 'rOut', 'Repro_Plots',"AllTaxa_Repro.csv")) 
+fileName<-file.path(file.path(baseDir, 'rOut', 'Repro_Plots',"AllTaxa_Repro.csv"))
 #########check ReadMe script to make sure this folder dependency is added in
-write.csv(repro, file = fileName,row.names=FALSE)
+write.csv(resultsByTaxon, file = fileName,row.names=FALSE)
 
 #Generate plot for each model
-model_i<-unique(repro$Model)
+model_i<-unique(resultsByTaxon$Model)
 col.l<-c("#D55E00", "#0072B2", "#009E73") #each model uses a different colour
 shp.l<-c(16,17,15)
 pd <- position_dodge(width = 0.4) ##for jitter
 for (i in 1:length(model_i)) {
 	mod.name=model_i[i]
-	repro.dat<-subset(repro,Model==mod.name) #plot each model separately
-	
+	repro.dat<-subset(resultsByTaxon,Model==mod.name) #plot each model separately
+
 	##define plotting parameters
 	y.max=max(repro.dat$Average)
 	index<-which.max(repro.dat$Average)
@@ -71,8 +85,8 @@ for (i in 1:length(model_i)) {
 	y.max<-y.max+st.dev
 	my.col<-c(col.l[i],"grey50")
 	my.shp<-shp.l[i]
-	
-	plot1<-ggplot(repro.dat, aes(x=UID, y=Average, group=StudyPeriod)) + 
+
+	plot1<-ggplot(repro.dat, aes(x=UID, y=Average, group=StudyPeriod)) +
 	geom_errorbar(aes(ymin=Average-stdev, ymax=Average+stdev),width=1.2,position = pd)+
 	#geom_point(shape=16,size=3,position = pd)+
 	geom_point(aes(color=StudyPeriod),size=1.5,shape=my.shp,position = pd)+
@@ -85,14 +99,14 @@ for (i in 1:length(model_i)) {
 	#theme_minimal()+
 	theme(legend.position="none",axis.line = element_line(colour = "black"),
     panel.border = element_blank(),
-    panel.background = element_blank(),panel.grid.minor=element_blank()) 
-	
-	p_name<-paste('Repro_',mod.name,'.png',sep='') 
+    panel.background = element_blank(),panel.grid.minor=element_blank())
+
+	p_name<-paste('Repro_',mod.name,'.png',sep='')
 	g_name<-paste('plot_',mod.name,sep='')
 	assign(g_name,plot1)
 	filePath <- file.path(file.path(baseDir, 'rOut','Repro_Plots'))
 	#tiff(filename = fileName,width=1200,height=800,units="px",pointsize=12,res=120)
-	ggsave(p_name, device = "png", path = filePath,scale = 1, width = 2500/300, height = 1770/300, units = 'in', dpi = 300, limitsize = TRUE)	
+	ggsave(p_name, device = "png", path = filePath,scale = 1, width = 2500/300, height = 1770/300, units = 'in', dpi = 300, limitsize = TRUE)
 
 rm(plot1,y.max,st.dev,index,repro.dat,g_name,p_name)
 }
@@ -100,8 +114,8 @@ rm(plot1,y.max,st.dev,index,repro.dat,g_name,p_name)
 #Calculate change in # of weeks between future & current conditions
 #Plot all models on same graph
 nn <- c('Taxa', 'UID','Model', 'Avg_Weeks')
-#change <- data.frame(matrix(NA, ncol = length(nn), nrow = nrow(repro)/2))
-change <- data.frame(matrix(NA, ncol = length(nn), nrow = nrow(repro)/2))
+#change <- data.frame(matrix(NA, ncol = length(nn), nrow = nrow(resultsByTaxon)/2))
+change <- data.frame(matrix(NA, ncol = length(nn), nrow = nrow(resultsByTaxon)/2))
 names(change) <- nn
 rm(nn)
 
@@ -109,20 +123,20 @@ y=seq(1,87,1)
 w=1
 
 ##Populate change dataframe that calculates the difference in the average number of weeks for each taxa-model combo
-for (j in 2:nrow(repro)){
-	if (repro$Taxa[j]==repro$Taxa[j-1] & repro$Model[j]==repro$Model[j-1]&repro$StudyPeriod[j]=="2029-2039"){
+for (j in 2:nrow(resultsByTaxon)){
+	if (resultsByTaxon$Taxa[j]==resultsByTaxon$Taxa[j-1] & resultsByTaxon$Model[j]==resultsByTaxon$Model[j-1]&resultsByTaxon$StudyPeriod[j]=="2029-2039"){
 		if (j==2){
 		w=w
 		}
 		else{
 		w=w+1
 		}
-		
+
 		z=y[w]
-		change$Taxa[z]<-repro$Taxa[j]
-		change$UID[z]<-repro$UID[j]
-		change$Model[z]<-repro$Model[j]
-		change$Avg_Weeks[z]<-repro$Average[j]-repro$Average[j-1]
+		change$Taxa[z]<-resultsByTaxon$Taxa[j]
+		change$UID[z]<-resultsByTaxon$UID[j]
+		change$Model[z]<-resultsByTaxon$Model[j]
+		change$Avg_Weeks[z]<-resultsByTaxon$Average[j]-resultsByTaxon$Average[j-1]
 		}
 	else {}
 	}
@@ -139,13 +153,13 @@ p4<-ggplot(change, aes(x=UID, y=Avg_Weeks, group=Model))+
 	theme_bw(base_size = 8)+
 	theme(legend.position="none",axis.line = element_line(colour = "black"),
     panel.border = element_blank(),
-    panel.background = element_blank(),panel.grid.minor=element_blank()) 
+    panel.background = element_blank(),panel.grid.minor=element_blank())
 
 	#Create 4-panel plot
 
 ##multiplot function from: http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
 ##Function is also copy-pasted at the bottom of this code
-p_name<-paste('Panel_Repro_Plot','.png',sep='') 
+p_name<-paste('Panel_Repro_Plot','.png',sep='')
 filePath <- file.path(file.path(baseDir, 'rOut','Repro_Plots'))
 p_n<-paste(filePath,p_name,sep='/')
 png(file=p_n,width = 2500/300, height = 2000/300,units = 'in', res = 300)
@@ -153,16 +167,16 @@ multiplot(plot_CCCma, plot_MIROC,plot_ECHOG,p4, cols=2) ##not working very well.
 dev.off()
 
 #Summary results - exclude species that cannot reproduce & two outliers that have nearly year-round repro
-repro.ex<-subset(repro,Average!=0 & Average<49)
+repro.ex<-subset(resultsByTaxon,Average!=0 & Average<49)
 repro.sum<-ddply(repro.ex,c("Model","StudyPeriod"),summarize,avg=mean(Average),std=sd(Average),N=length(Average))
-fileName<-file.path(file.path(baseDir, 'rOut', 'Repro_Plots',"Repro_Summary.csv")) 
+fileName<-file.path(file.path(baseDir, 'rOut', 'Repro_Plots',"Repro_Summary.csv"))
 write.csv(repro.sum, file = fileName,row.names=FALSE)
 
 #Clean up workspace
 rm(r,cs,repro.clip,s_OK.Repro,fileName,filePath,name,pd,mod.name,i,ff,f,col.l,my.col,shp.l,w,x,y,z,p_name,p_n,my.shp,p4,j,plot_CCCma,plot_ECHOG,plot_MIROC,model_i,ymin,)
 
 ##remove dataframes
-rm(change,repro)
+rm(change,resultsByTaxon)
 
 
 
