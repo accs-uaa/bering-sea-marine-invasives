cb6223b3e089dee5ebedf5f08113eb08d63a1998
Amanda Droghini <adroghini@alaska.edu>
2019-11-12 14:57:55 -0900

    Update 00-beringSeaBathymetry.R
    Include work-around for marmap antimeridian problem.

--- rCode/00-beringSeaBathymetry.R
+++ rCode/00-beringSeaBathymetry.R
@@ -5,25 +5,43 @@
 ## Source initial run script
 source('rCode/init.R')
 
-## Define the study area polygon using geographic coordinates 
-xLimits<-sort(c(150, -135))					# (eastings WGS84)
-yLimits<-sort(c(40, 70))					# (northings WGS84)
-
-## Define projections
-prj.StudyArea <- CRS("+proj=aeqd +lat_0=70 +lon_0=-170 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0") 
-prj.StudyAreaPositive <- CRS("+proj=aeqd +lat_0=70 +lon_0=190") # +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0") #Lambert -170, 70
-prj.geo<-CRS('+proj=longlat +datum=WGS84')
 
 ## Generate bathymetry raster
-bath <- getNOAA.bathy(xLimits[1]+15, xLimits[2]-10, yLimits[1]-5, yLimits[2]+10, resolution = 1, antimeridian = TRUE, keep =T)
-bath.r.geo<-marmap::as.raster(bath)
-bath.r.1<-projectRaster(from=bath.r.geo, res=2000, crs=prj.StudyAreaPositive, method="bilinear", over=TRUE)
-bath.r<-projectRaster(from=bath.r.1, res=2000, crs=prj.StudyArea, method="bilinear", over=TRUE)
 
-bath200<-rasterToContour(bath.r, levels=-200) # Extract only areas <= 200 m depth
-rgdal::writeOGR(bath200, 'rData', "bath200", driver="ESRI Shapefile") # Export for use in GIS
-save(bath.r, bath200, file=BathymetryDataFile) # Export as .Rdata for use in R
+## Note: Due to a problem with antimeridian in current r version, I have run this portion of the script on an old R version (v. 3.2.5). This requires attention. 
+## The solution to the previous post addressing projection of rasters across the antimeridian (http://r-sig-geo.2731867.n2.nabble.com/coercing-marmap-bathy-object-to-raster-spanning-the-antimeridian-td7589828.html#a7589841) no longer works (as of 24 June 2016).
 
-unloadNamespace('marmap')
+## The ETOPO1.rData file can be accessed on our repository @ KNB Ecoinformatics.
 
-rm(list=ls())
+BathymetryDataFile <- file.path(baseDir, 'rData/ETOPO1.rData')
+
+if(!file.exists(BathymetryDataFile)){
+  
+  ## Define the study area polygon using geographic coordinates 
+  xLimits <- sort(c(170, -135)) ## define the study area polygon using geographic coordinates (eastings WGS84)
+  yLimits <- sort(c(52, 75)) ## (northings WGS84)
+  
+  ## Define projections
+  prj.StudyArea <- CRS("+proj=aeqd +lat_0=70 +lon_0=-170 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0") 
+  prj.StudyAreaPositive <- CRS("+proj=aeqd +lat_0=70 +lon_0=190") # +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0") #Lambert -170, 70
+  prj.geo<-CRS('+proj=longlat +datum=WGS84')
+  
+  
+  bath <- getNOAA.bathy(xLimits[1]+15, xLimits[2]-10, yLimits[1]-5, yLimits[2]+10, resolution = 1, antimeridian = TRUE, keep =T)
+  bath.r.geo<-marmap::as.raster(bath)
+  bath.r.1<-projectRaster(from=bath.r.geo, res=2000, crs=prj.StudyAreaPositive, method="bilinear", over=TRUE)
+  bath.r<-projectRaster(from=bath.r.1, res=2000, crs=prj.StudyArea, method="bilinear", over=TRUE)
+  
+  bath200 <- rasterToContour(bath.r, levels=-200) # Extract only areas <= 200 m depth
+  save(bath.r, bath200, file=BathymetryDataFile) # Export as .Rdata for use in R
+  
+  unloadNamespace('marmap')
+  rm(bath, bath.r.geo, bath.r.1,xLimits,yLimits)
+  
+}else{
+  load(BathymetryDataFile)
+
+}
+
+# Export as shapefile to generate Figure 1 in ESRI ArcMap
+rgdal::writeOGR(bath200, 'rData', "bath200", driver="ESRI Shapefile") 
